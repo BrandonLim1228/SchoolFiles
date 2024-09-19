eb2c8da8d1f795d174ae431c76094d1e600784a9
Brandon Lim <brandonlim1228@gmail.com>
2024-09-18 23:37:57 -0600

    9/18/24 1137pm from PC

--- 2024/Fall 2024/Classical Controls/Homework 4/Homework4Script.m
+++ 2024/Fall 2024/Classical Controls/Homework 4/Homework4Script.m
@@ -2,16 +2,20 @@
 %Homework 4
 clear, clc, close all
 
-Gone = tf([1,2],[1,3,2]);
-Gtwo = tf([-1,2],[1,3,2]);
+%Problem 1
+    Gone = tf([1,2],[1,3,2]);
+    Gtwo = tf([-1,2],[1,3,2]);
 
-figure
-    step(Gone)
-    title("Step Response of G_1(s)")
-figure
-    step(Gtwo)
-    title("Step Response of G_2(s)")
-figure
-    step(tf([1],[1,1]))
-figure
-    step(tf([3],[1,1])+tf([-4],[1,2]))
+    figure
+        step(Gone)
+        title("Step Response of G_1(s)")
+    figure
+        step(Gtwo)
+        title("Step Response of G_2(s)")
+
+%Problem 5
+    Gcl = tf([(9/4)],[1,3,(9/4)])
+
+    figure
+        step(Gcl)
+        title("Fastest Possible Step Response without Oscillations of Gcl with K = (9/4)")
