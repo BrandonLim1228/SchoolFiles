f6e712f4f96592356b0bc9c3adf2b92fb0c4d3ed
Brandon Lim <brandonlim1228@gmail.com>
2024-10-23 22:16:13 -0600

    102324 1016PM from PC

--- 2024/Fall 2024/Classical Controls/Homework 6/Homework6Script.m
+++ 2024/Fall 2024/Classical Controls/Homework 6/Homework6Script.m
@@ -4,7 +4,7 @@ clear, clc, close all
 
 %Problem 1
     %Initialzing variable
-        t = linspace(0,100,100);
+        t = linspace(0,50,1000);
     %Input 
         r = zeros(size(t));
         r(0 <= t & t < 10) = t(0 <= t & t < 10);
@@ -13,9 +13,18 @@ clear, clc, close all
         r(30 <= t & t < 40) = 10-r(0 <= t & t < 10);
     %Transfer function
         Gs = tf([75 75],[1 30 200 75])
+        Es = tf([1 30 125 0], [1 30 200 75])
     %Plotting time response of the output of the close-loop system (a)
         figure
-        lsim(Gs,r,t)
+        [y1,tOut1] = lsim(Gs,r(1:800),t(1:800));
+        plot(tOut1,y1)
+        xlabel("Time [Seconds]")
+        ylabel("Output")
+        title("Time response of the Output of the Closed-Loop System")
     %Plotting tracking error as a function of time
         figure
-        lsim(1/(1+Gs),r,t)
+        [y2,tOut2] = lsim(Es,r(1:800),t(1:800))
+        plot(tOut2,y2)
+        xlabel("Time [Seconds]")
+        ylabel("Tracking Error")
+        title("Time response of the Tracking Error")
