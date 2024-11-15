7ab05174bfdc327f5a7fa7b793ea58d28edabda5
Brandon Lim <brandonlim1228@gmail.com>
2024-11-14 22:21:55 -0700

    111424 1021PM from PC

--- 2024/Fall 2024/Classical Controls/Homework 8/Homework8Script.m
+++ 2024/Fall 2024/Classical Controls/Homework 8/Homework8Script.m
@@ -27,32 +27,38 @@ clear, clc, close all
 
         k = linspace(0,7.34,1000);
         best_k = 0;
-        go = 1;
+
         for i = 1:length(k)
             Gcl(i) = tf([k(i) ,k(i)*1.5], [1 11 (10+k(i)) (k(i)*1.5)]);
             overshoot = stepinfo(Gcl(i)).Overshoot;
             if overshoot < 1.522 && overshoot > 1.517
                 best_k = k(i)
+                overshoot
             end
         end
 
-%% Problem 3
+%% Problem 3 a
 clear, clc, close all
     %1a)
         Dc = tf([80 1.8*80], [1 22])
         G = tf([1], [1 0 0])
         Gcl = feedback(Dc*G,1)
+
         figure
-        step(Gcl)
+            step(Gcl)
         figure
-        step(G)
+            step(G)
         figure
-        rlocus(G)
+            rlocus(Gcl)
+
         stepinfo(Gcl)
 
-        roots([1 22 80 144])
-
-%%
-C = tf([1, 0.2], [1, 0.08469]);
-G = tf(1, [1, 2, 0]);
-rlocus(C*G)
+%% Problem 3 b
+clear, clc, close all
+ Gs = tf([1], [1 2 0]);
+ Dc = tf([2.077 2.077*0.05], [1 0.013]);
+ Gol = Gs * Dc
+ Gcl = feedback(Gol,1)
+ rlocus(Gol)
+ roots([1 2.013 2.013 0.1038])
+    
