bb5f886b311c99b3eb71f1fea348c2493a30bf25
Brandon Lim <brandonlim1228@gmail.com>
2024-11-18 22:46:31 -0700

    111824 1046Pm from PC

--- 2024/Fall 2024/Classical Controls/Homework 8/Homework8Script.m
+++ 2024/Fall 2024/Classical Controls/Homework 8/Homework8Script.m
@@ -1,64 +1,91 @@
 %Brandon Lim
 clear, clc, close all
- 
+
 %% Problem 1
 clear, clc, close all
-    %1a)
-        sys = tf([1,3,2],[1 -3 2])
-        rlocus(sys)
-    %1b)
-        [K, POLES] = rlocfind(sys)
+%1a)
+sys = tf([1,3,2],[1 -3 2])
+rlocus(sys)
+%1b)
+[K, POLES] = rlocfind(sys)
 %% Problem 2
 clear, clc, close all
-    %1a)
-        sys = tf([1 1.5], [1 11 10 0]);
-        rlocus(sys,0:0.005:50);
-        sgrid(0.799839691078,0)
-    %1d)
-        k = [39.6 12.8 7.34];
-        Gcl1 = tf([k(1) ,k(1)*1.5], [1 11 (10+k(1)) (k(1)*1.5)]);
-        Gcl2 = tf([k(2) ,k(2)*1.5], [1 11 (10+k(2)) (k(2)*1.5)]);
-        Gcl3 = tf([k(3) ,k(3)*1.5], [1 11 (10+k(3)) (k(3)*1.5)]);
+%1a)
+sys = tf([1 1.5], [1 11 10 0]);
+rlocus(sys,0:0.005:50);
+sgrid(0.799839691078,0)
+%1d)
+k = [39.6 12.8 7.34];
+Gcl1 = tf([k(1) ,k(1)*1.5], [1 11 (10+k(1)) (k(1)*1.5)]);
+Gcl2 = tf([k(2) ,k(2)*1.5], [1 11 (10+k(2)) (k(2)*1.5)]);
+Gcl3 = tf([k(3) ,k(3)*1.5], [1 11 (10+k(3)) (k(3)*1.5)]);
 
 
-        OvershootK396 = stepinfo(Gcl1).Overshoot;
-        OvershootK128 = stepinfo(Gcl2).Overshoot;
-        OvershootK734 = stepinfo(Gcl3).Overshoot;
+OvershootK396 = stepinfo(Gcl1).Overshoot;
+OvershootK128 = stepinfo(Gcl2).Overshoot;
+OvershootK734 = stepinfo(Gcl3).Overshoot;
 
-        k = linspace(0,7.34,1000);
-        best_k = 0;
+k = linspace(0,7.34,1000);
+best_k = 0;
 
-        for i = 1:length(k)
-            Gcl(i) = tf([k(i) ,k(i)*1.5], [1 11 (10+k(i)) (k(i)*1.5)]);
-            overshoot = stepinfo(Gcl(i)).Overshoot;
-            if overshoot < 1.522 && overshoot > 1.517
-                best_k = k(i)
-                overshoot
-            end
-        end
+for i = 1:length(k)
+    Gcl(i) = tf([k(i) ,k(i)*1.5], [1 11 (10+k(i)) (k(i)*1.5)]);
+    overshoot = stepinfo(Gcl(i)).Overshoot;
+    if overshoot < 1.522 && overshoot > 1.517
+        best_k = k(i)
+        overshoot
+    end
+end
 
 %% Problem 3 a
 clear, clc, close all
-    %1a)
-        Dc = tf([80 1.8*80], [1 22])
-        G = tf([1], [1 0 0])
-        Gcl = feedback(Dc*G,1)
+%1a)
+Dc = tf([80 1.8*80], [1 22])
+G = tf([1], [1 0 0])
+Gcl = feedback(Dc*G,1)
 
-        figure
-            step(Gcl)
-        figure
-            step(G)
-        figure
-            rlocus(Gcl)
+figure
+step(Gcl)
+figure
+step(G)
+figure
+rlocus(Gcl)
 
-        stepinfo(Gcl)
+stepinfo(Gcl)
 
 %% Problem 3 b
 clear, clc, close all
- Gs = tf([1], [1 2 0]);
- Dc = tf([2.077 2.077*0.05], [1 0.013]);
- Gol = Gs * Dc
- Gcl = feedback(Gol,1)
- rlocus(Gol)
- roots([1 2.013 2.013 0.1038])
-    
+Gs = tf([1], [1 2 0]);
+Dc = tf([2.08 2.08*0.1], [1 0.0208]);
+Gcl = feedback(Gs * Dc,1);
+
+t = linspace(0,10,100);
+u(0 <= t & t <= 1) = t(0 <= t & t <= 1);
+u(1 <= t & t <= 10) = 1;
+
+[yGcl,t] = lsim(Gcl,u,t);
+[yGol,t] = lsim(Gs,u,t);
+
+figure
+plot(t,yGcl)
+hold on
+plot(t,u)
+legend("Ramp Response","Ramp Input","Location","southeast")
+xlabel("Time [sec]")
+ylabel("Output")
+title("Unit Ramp Response of Gcl(s)")
+
+figure
+plot(t,yGol)
+hold on
+plot(t,u)
+legend("Ramp Response","Ramp Input","Location","southeast")
+xlabel("Time [sec]")
+ylabel("Output")
+title("Unit Ramp Response of Gol(s)")
+%% Problem 4
+clear, clc, close all
+    Gol = tf([1], [1 12.828 44.28 160 0])
+    Gcl = tf([96.775 387.1 387.1],[1 12.828 (44.28+96.775) (160+387.1) 387.1])
+    step(Gol)
+    step(Gcl)
