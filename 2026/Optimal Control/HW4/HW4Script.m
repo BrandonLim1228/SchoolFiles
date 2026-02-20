ae27ad13a12877ee0583e97ca996774b5e4b2fac
Brandon Lim FPC lab computer <Brandonlim1228@gmail.com>
2026-02-20 16:50:52 -0700

    022026 450pm from fpc lab

--- 2026/Optimal Control/HW4/HW4Script.m
+++ 2026/Optimal Control/HW4/HW4Script.m
@@ -1,77 +1,37 @@
 clear, clc, close all
 
-syms x1(t) x2(t) l1(t) l2(t)
+% Time mesh (integral bounds of time to evaluate solution over)
+tmesh = linspace(0, pi/2, 100);
 
-ode1 = diff(x1) == x2;
-ode2 = diff(x2) == -9*l2-2*x1;
-ode3 = diff(l1) == 2*l2;
-ode4 = diff(l2) == -l1;
-odes = [ode1;ode2;ode3;ode4]
+% Initial guess mapped onto time mesh (Can be anything, better guesses will just save computation resources)
+solinit = bvpinit(tmesh, [0 0 0 0]);
 
-s = dsolve(odes)
-%%
-syms u(t) v(t)
-ode1 = diff(u) == 3*u + 4*v;
-ode2 = diff(v) == -4*u + 3*v;
-odes = [ode1; ode2]
-S = dsolve(odes)
+% Solve BVP
+sol = bvp4c(@bvpfcn, @bcfun, solinit);
 
-%%
-clear,clc
+% ODE Function
+function dxdt = bvpfcn(t,x)
+    A = [ 0  1  0  0;
+        -2  0  0 -9;
+        0  0  0  2;
+        0  0 -1  0];
+    dxdt = A*x;
+end
 
-syms l1(t) l2(t)
-ode1 = diff(l1) == 2*l2;
-ode2 = diff(l2) == -l1;
-odes = [ode1; ode2]
-S = dsolve(odes)
-%%
-clear,clc
-
-%State equations
-syms x1 x2 p1 p2 u;
-Dx1 = x2;
-Dx2 = -2*x1+3*u;
-
-syms v;
-v = 0.5*u^2;
-
-syms p1 p2 H;
-H = v + p1*Dx1 + p2*Dx2;
-
-Dp1 = -diff(H,x1);
-Dp2 = -diff(H,x2);
-du = diff(H,u);
-
-sol_u = solve(du, u);
-
-Dx2 = subs(Dx2,u,sol_u);
-
-% eq1 = strcat('Dx1 = ', Dx1);
-% eq2 = strcat('Dx2 = ', Dx2);
-% eq3 = strcat('Dp1 = ', Dp1);
-% eq4 = strcat('Dp2 = ', Dp2);
-
-sol_h = dsolve(Dx1,Dx2,Dp1,Dp2)
-
-%%
-clear, clc
-
-A = [0 1 0 0; -2 0 0 0; 0 0 0 2; 0 0 -1 0];
-[V,D] = eig(A)
-%%
-clear,clc
-
-A = [0 1 0 0; -2 0 0 0; 0 0 0 2; 0 0 -1 0];
-
-f = @(t,x) A*x;
-[t,X] = ode45(f,[0 pi/2],[0;1;1;1]);
+%Boundary Condition Function
+function res = bcfun(xa,xb)
+    % Boundary Condition vector
+    res = zeros(4,1);
+    % Fromatted via matlab guidelines for bvp4c (Boundary conditions set equal to zero)
+    % Initial conditions
+    res(1) = xa(1);          % x1(0) = 0
+    res(2) = xa(2) - 1;      % x2(0) = 1
+    % Final conditions
+    res(3) = xb(1) - xb(3);  % l1(tf) = x1(tf) - l1(tf) = 0
+    res(4) = xb(4);          % l2(tf) = 0
+end
 
 figure
-plot(t,X(:,1))
-hold on
-plot(t,X(:,2))
-
-figure
-plot(t,X(:,3))
-hold on
-plot(t,X(:,4))
+plot(sol.x,sol.y)
+legend("$x_1$","$x_2$", "$\lambda_1$", "$\lambda_2$", "Interpreter","latex")
+xlabel("Time"); ylabel("State Value"); title("Optimal Control States vs Time")
