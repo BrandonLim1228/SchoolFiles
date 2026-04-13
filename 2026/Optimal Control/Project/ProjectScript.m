e9eea3ea15feca288694facbf137a080b3b1175c
BrandonLim <brandonlim1228@gmail.com>
2026-04-13 12:49:11 -0600

    41326 1248pm from laptop

--- 2026/Optimal Control/Project/ProjectScript.m
+++ 2026/Optimal Control/Project/ProjectScript.m
@@ -8,102 +8,102 @@ clear, clc, close all
 %Inputs (In order):
     % eta - aileron deflection angle (radians)
     % zeta - rudder deflection angel (radians)
-A = [0 0 1 0.078; 
-    0.064 -0.202 0.078 -0.99;
-    0 -22.92 -2.25 0.54;
-    0 6 -0.04 -0.31];
-B = [0 0;
-    0.0002 0.0005;
-    -0.4623 0.0569;
-    -0.0244 -0.0469];
-C = (180/pi)* eye(4);
-D = [0 0;
-    0 0;
-    0 0;
-    0 0];
-
-sys = ss(A,B,C,D);
-
-x0 = [0;0;0;0]; % Initial States
-t = linspace(0,10,1000)'; % Roll time
-eta = deg2rad(-20) * ones(length(t),1); %Roll input of 5 degrees aleron deflection
-% c = [0.001791 -0.024 0.18 -0.086 -12.1];
-% eta = deg2rad(polyval(c,t));
-zeta = zeros(length(t),1);
-u = [eta, zeta];
-figure
-plot(t,eta)
-
-[y,t,x] = lsim(sys,u,t,x0);
-
-psi_hist = cumtrapz(t, y(:,4));   % integrate yaw rate
-
-figure
-plot(t,y)
-hold on
-plot(t,psi_hist)
-title("Pure Aileron Deflection to control roll"); xlabel("Time"); ylabel("Degrees or Degrees/sec")
-legend("Roll Angle", "Side Slip Angle", "Roll Rate", "Yaw Rate", "Yaw Angle");
-
-
-Rx = @(phi)[1 0 0;
-            0 cosd(phi) -sind(phi);
-            0 sind(phi) cosd(phi)];
-
-Rz = @(psi)[cosd(psi) -sind(psi) 0;
-            sind(psi)  cosd(psi) 0;
-            0         0        1];
-
-% Simple airplane geometry in body axes
-% x forward, y right, z down
-
-fuselage = [-0.8 1.0;   0   0;    0   0];
-wing     = [ 0   0;   -0.8 0.8;   0   0];
-htail    = [-0.5 -0.5; -0.25 0.25; 0 0];
-vtail    = [-0.5 -0.5;  0    0;   0 0.25];
-
-figure
-axis equal
-grid on
-xlabel('X'), ylabel('Y'), zlabel('Z')
-xlim([-2 2]), ylim([-2 2]), zlim([-2 2])
-view(3)
-
-for k = 1:10:length(t)
-
-    phi_k = y(k,1);
-    psi_k = psi_hist(k);
-
-    R = Rz(psi_k) * Rx(phi_k);
-
-    fus = R * fuselage;
-    wng = R * wing;
-    htl = R * htail;
-    vtl = R * vtail;
-
-    cla
-    plot3(fus(1,:), fus(2,:), fus(3,:), 'LineWidth', 2)
-    hold on
-    plot3(wng(1,:), wng(2,:), wng(3,:), 'LineWidth', 2)
-    plot3(htl(1,:), htl(2,:), htl(3,:), 'LineWidth', 2)
-    plot3(vtl(1,:), vtl(2,:), vtl(3,:), 'LineWidth', 2)
-
-    plot3(0,0,0,'o')   % center marker
-
-    axis equal
-    xlim([-1.2 1.2])
-    ylim([-1.2 1.2])
-    zlim([-1.2 1.2])
-    view(35,25)
-    grid on
-
-    title(sprintf('t = %.2f s', t(k)))
-    xlabel('X')
-    ylabel('Y')
-    zlabel('Z')
-
-    drawnow
-end
+% A = [0 0 1 0.078; 
+%     0.064 -0.202 0.078 -0.99;
+%     0 -22.92 -2.25 0.54;
+%     0 6 -0.04 -0.31];
+% B = [0 0;
+%     0.0002 0.0005;
+%     -0.4623 0.0569;
+%     -0.0244 -0.0469];
+% C = (180/pi)* eye(4);
+% D = [0 0;
+%     0 0;
+%     0 0;
+%     0 0];
+% 
+% sys = ss(A,B,C,D);
+% 
+% x0 = [0;0;0;0]; % Initial States
+% t = linspace(0,10,1000)'; % Roll time
+% eta = deg2rad(-20) * ones(length(t),1); %Roll input of 5 degrees aleron deflection
+% % c = [0.001791 -0.024 0.18 -0.086 -12.1];
+% % eta = deg2rad(polyval(c,t));
+% zeta = zeros(length(t),1);
+% u = [eta, zeta];
+% figure
+% plot(t,eta)
+% 
+% [y,t,x] = lsim(sys,u,t,x0);
+% 
+% psi_hist = cumtrapz(t, y(:,4));   % integrate yaw rate
+% 
+% figure
+% plot(t,y)
+% hold on
+% plot(t,psi_hist)
+% title("Pure Aileron Deflection to control roll"); xlabel("Time"); ylabel("Degrees or Degrees/sec")
+% legend("Roll Angle", "Side Slip Angle", "Roll Rate", "Yaw Rate", "Yaw Angle");
+% 
+% 
+% Rx = @(phi)[1 0 0;
+%             0 cosd(phi) -sind(phi);
+%             0 sind(phi) cosd(phi)];
+% 
+% Rz = @(psi)[cosd(psi) -sind(psi) 0;
+%             sind(psi)  cosd(psi) 0;
+%             0         0        1];
+% 
+% % Simple airplane geometry in body axes
+% % x forward, y right, z down
+% 
+% fuselage = [-0.8 1.0;   0   0;    0   0];
+% wing     = [ 0   0;   -0.8 0.8;   0   0];
+% htail    = [-0.5 -0.5; -0.25 0.25; 0 0];
+% vtail    = [-0.5 -0.5;  0    0;   0 0.25];
+% 
+% figure
+% axis equal
+% grid on
+% xlabel('X'), ylabel('Y'), zlabel('Z')
+% xlim([-2 2]), ylim([-2 2]), zlim([-2 2])
+% view(3)
+% 
+% for k = 1:10:length(t)
+% 
+%     phi_k = y(k,1);
+%     psi_k = psi_hist(k);
+% 
+%     R = Rz(psi_k) * Rx(phi_k);
+% 
+%     fus = R * fuselage;
+%     wng = R * wing;
+%     htl = R * htail;
+%     vtl = R * vtail;
+% 
+%     cla
+%     plot3(fus(1,:), fus(2,:), fus(3,:), 'LineWidth', 2)
+%     hold on
+%     plot3(wng(1,:), wng(2,:), wng(3,:), 'LineWidth', 2)
+%     plot3(htl(1,:), htl(2,:), htl(3,:), 'LineWidth', 2)
+%     plot3(vtl(1,:), vtl(2,:), vtl(3,:), 'LineWidth', 2)
+% 
+%     plot3(0,0,0,'o')   % center marker
+% 
+%     axis equal
+%     xlim([-1.2 1.2])
+%     ylim([-1.2 1.2])
+%     zlim([-1.2 1.2])
+%     view(35,25)
+%     grid on
+% 
+%     title(sprintf('t = %.2f s', t(k)))
+%     xlabel('X')
+%     ylabel('Y')
+%     zlabel('Z')
+% 
+%     drawnow
+% end
 
 %% Optimal Control 
 %Figure Defaults
@@ -114,7 +114,6 @@ set(groot, 'defaultColorbarTickLabelInterpreter', 'latex')
 set(groot, 'defaultAxesFontSize', 17)
 set(groot, 'defaultAxesFontWeight', 'bold')
 set(groot, 'defaultFigureColor', [1.0 1.0 1.0])
-set(groot, 'defaultFigurePosition', [450 85 1176 864])
 
 clear, clc, close all
 %States (In order):
@@ -141,10 +140,10 @@ D = [0 0;
 sys = ss(A,B,C,D);
 
 %Cost Function Weightings
-F = [1 0 0 0;
+F = [500 0 0 0;
     0 0 0 0;
     0 0 0 0;
-    0 0 0 1];
+    0 0 0 10];
 Q = [1 0 0 0;
     0 0 0 0;
     0 0 0 0;
@@ -190,7 +189,8 @@ for i = 1:length(t)
     u(:,i) = u_current;
 end
 
-psi = cumtrapz(t, x(4,:)); % integrate yaw rate
+%Integrating yaw rate to get yaw
+psi = cumtrapz(t, x(4,:)); 
 
 
 z = [3*t; zeros(length(t),1)'; zeros(length(t),1)'; zeros(length(t),1)'];
